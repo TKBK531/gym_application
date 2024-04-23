4b9f2ba1cdf549f23966fb94380720e9030b1320
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-04-23 16:51:03 +0530

    WIP

--- frontend/src/pages/LeftBar.jsx
+++ frontend/src/pages/LeftBar.jsx
@@ -2,6 +2,15 @@ import React from "react";
 
 const LeftBar = () => (
   <div>
+    <div className="items-center text-center">
+      <h1 className="font-poppins text-[30px]">University Gymnasium</h1>
+      <img
+        className="mt-10 w-28 h-28 rounded-full mx-auto"
+        src="https://picsum.photos/200"
+      />
+      <h1 className="font-bold">Jenny Wilson</h1>
+      <h3 className="text-xs text-gray-500">jennywilson@gmail.com</h3>
+    </div>
     <h1>LeftBar</h1>
   </div>
 );
