39f642f17d882b74ad7618d62d50853629e42317
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-04-23 17:06:48 +0530

    WIP

--- frontend/src/pages/LeftBar.jsx
+++ frontend/src/pages/LeftBar.jsx
@@ -1,13 +1,14 @@
+import AvatarOnline from "../components/AvatarOnline";
 import React from "react";
 
 const LeftBar = () => (
   <div>
     <div className="items-center text-center">
       <h1 className="font-poppins text-[30px]">University Gymnasium</h1>
-      <img
-        className="mt-10 w-28 h-28 rounded-full mx-auto"
-        src="https://picsum.photos/200"
-      />
+      <div className="flex justify-center">
+        <AvatarOnline image="https://picsum.photos/200/300" />
+      </div>
+
       <h1 className="font-bold">Jenny Wilson</h1>
       <h3 className="text-xs text-gray-500">jennywilson@gmail.com</h3>
     </div>
