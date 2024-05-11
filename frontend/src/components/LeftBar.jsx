aef28bdefbe6b67214fea3bff4896e591c8b061d
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-11 21:48:18 +0530

    FIX: Styled the Left Bar

--- frontend/src/components/LeftBar.jsx
+++ frontend/src/components/LeftBar.jsx
@@ -4,27 +4,30 @@ import { pageLinks } from "../constants";
 import PageLink from "./NavComponents/PageLink";
 
 const LeftBar = () => (
-  <div className=" w-[290px]">
+  <div className=" w-[290px] bg-info-dark-blue h-screen">
     <div className="items-center text-center px-[70px] py-[40px]">
       {/* <h1 className="font-poppins text-[30px]">University Gymnasium</h1> */}
       <div className="flex justify-center">
         <AvatarOnline image="https://picsum.photos/200/300" />
       </div>
       <div className="p-1">
-        <h1 className="font-medium text-[16px]">Alesia K.</h1>
+        <h1 className="font-medium text-[16px] text-white">Alesia K.</h1>
         <h3 className="font-semibold text-gray-500 text-[12px]">
           Basic Member
         </h3>
       </div>
     </div>
-    {pageLinks.map((link) => (
-      <PageLink
-        key={link.index}
-        href={link.href}
-        icon={link.icon}
-        name={link.name}
-      />
-    ))}
+    <hr className="mb-10" />
+    <div>
+      {pageLinks.map((link) => (
+        <PageLink
+          key={link.index}
+          href={link.href}
+          icon={link.icon}
+          name={link.name}
+        />
+      ))}
+    </div>
   </div>
 );
 
--- frontend/src/components/NavComponents/PageLink.jsx
+++ frontend/src/components/NavComponents/PageLink.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLocation } from "react-router-dom";
 
-const PageLink = ({ name, icon, href }) => (
-  <div className="pl-3 bg-purple-300 my-3">
-    <a href={href} className="flex">
-      <div className="m-1">
-        <FontAwesomeIcon icon={icon} />
-      </div>
-      <div className="mr-auto m-1">
-        <p>{name}</p>
-      </div>
-      <div className="m-1 mr-0">
-        <p>|</p>
-      </div>
-    </a>
-  </div>
-);
+const PageLink = ({ name, icon, href }) => {
+  const location = useLocation();
+  const isActive = location.pathname === href;
+
+  return (
+    <div
+      className={`pl-3 my-3 ${
+        isActive
+          ? "text-secondary-golden font-semibold"
+          : "text-white font-light"
+      }`}
+    >
+      <a href={href} className="flex">
+        <div className="m-1">
+          <FontAwesomeIcon icon={icon} className="h-4 w-4 mr-2" />
+        </div>
+        <div className="mr-auto m-1">
+          <p>{name}</p>
+        </div>
+      </a>
+    </div>
+  );
+};
 
 export default PageLink;
