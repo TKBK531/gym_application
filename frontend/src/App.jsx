1cefe6aeb25f3d4a63bfd794661941909a3cb7b7
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-12 22:11:07 +0530

    FIX: fixed the project to use QueryClientProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 import {
   BaseLayout,
@@ -94,11 +95,15 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient(); // Define the queryClient variable
+
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    </QueryClientProvider>
   );
 }
 
