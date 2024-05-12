06ccb0254190747d03a640b3a0aa868580a9b8c7
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-12 21:06:30 +0530

    FEAT: Add the functionality to fetch info for the leftbar from the backend

--- frontend/src/components/LeftBar.jsx
+++ frontend/src/components/LeftBar.jsx
@@ -2,18 +2,35 @@ import React from "react";
 import AvatarOnline from "./NavComponents/AvatarOnline";
 import { pageLinks } from "../constants";
 import PageLink from "./NavComponents/PageLink";
+import { useQuery } from "@tanstack/react-query";
+
+async function fetchUserProfile() {
+  const response = await fetch("/api/user/profile");
+  if (!response.ok) {
+    throw new Error("Network response was not ok.");
+  }
+  return response.json();
+}
 
 const LeftBar = () => {
+  const {
+    isLoading,
+    error,
+    data: userProfile,
+  } = useQuery("profile", fetchUserProfile);
+
   return (
     <div className=" w-[290px] bg-info-dark-blue h-screen">
       <div className="items-center text-center px-[70px] py-[40px]">
         <div className="flex justify-center">
-          <AvatarOnline image="https://picsum.photos/200/300" />
+          <AvatarOnline image={userProfile.profilePictureUrl} />
         </div>
         <div className="p-1">
-          <h1 className="font-medium text-[16px] text-white">Alesia K.</h1>
+          <h1 className="font-medium text-[16px] text-white">
+            {userProfile.name}
+          </h1>
           <h3 className="font-semibold text-gray-500 text-[12px]">
-            Basic Member
+            {userProfile.profileStatus}
           </h3>
         </div>
       </div>
