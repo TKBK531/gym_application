42a0498d5684726e6a66b35fdc20263fb51f9a14
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-12 09:07:31 +0530

    WIP

--- frontend/src/components/LeftBar.jsx
+++ frontend/src/components/LeftBar.jsx
@@ -3,31 +3,33 @@ import AvatarOnline from "./NavComponents/AvatarOnline";
 import { pageLinks } from "../constants";
 import PageLink from "./NavComponents/PageLink";
 
-const LeftBar = () => (
-  <div className=" w-[290px] bg-info-dark-blue h-screen">
-    <div className="items-center text-center px-[70px] py-[40px]">
-      <div className="flex justify-center">
-        <AvatarOnline image="https://picsum.photos/200/300" />
+const LeftBar = () => {
+  return (
+    <div className=" w-[290px] bg-info-dark-blue h-screen">
+      <div className="items-center text-center px-[70px] py-[40px]">
+        <div className="flex justify-center">
+          <AvatarOnline image="https://picsum.photos/200/300" />
+        </div>
+        <div className="p-1">
+          <h1 className="font-medium text-[16px] text-white">Alesia K.</h1>
+          <h3 className="font-semibold text-gray-500 text-[12px]">
+            Basic Member
+          </h3>
+        </div>
       </div>
-      <div className="p-1">
-        <h1 className="font-medium text-[16px] text-white">Alesia K.</h1>
-        <h3 className="font-semibold text-gray-500 text-[12px]">
-          Basic Member
-        </h3>
+      <hr className="mb-10" />
+      <div>
+        {pageLinks.map((link) => (
+          <PageLink
+            key={link.index}
+            href={link.href}
+            icon={link.icon}
+            name={link.name}
+          />
+        ))}
       </div>
     </div>
-    <hr className="mb-10" />
-    <div>
-      {pageLinks.map((link) => (
-        <PageLink
-          key={link.index}
-          href={link.href}
-          icon={link.icon}
-          name={link.name}
-        />
-      ))}
-    </div>
-  </div>
-);
+  );
+};
 
 export default LeftBar;
