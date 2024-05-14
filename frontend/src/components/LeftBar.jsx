fbf67be8c0459f006da3c0dcbb45444ca9e8266f
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-14 12:33:35 +0530

    FIX: Fixed leftbar styles

--- frontend/src/components/LeftBar.jsx
+++ frontend/src/components/LeftBar.jsx
@@ -2,35 +2,20 @@ import React from "react";
 import AvatarOnline from "./NavComponents/AvatarOnline";
 import { pageLinks } from "../constants";
 import PageLink from "./NavComponents/PageLink";
-import { useQuery } from "@tanstack/react-query";
-
-async function fetchUserProfile() {
-  const response = await fetch("/api/user/profile");
-  if (!response.ok) {
-    throw new Error("Network response was not ok.");
-  }
-  return response.json();
-}
 
 const LeftBar = () => {
-  const {
-    isLoading,
-    error,
-    data: userProfile,
-  } = useQuery("profile", fetchUserProfile);
-
   return (
     <div className=" w-[290px] bg-info-dark-blue h-screen">
-      <div className="items-center text-center px-[70px] py-[40px]">
+      <div className="items-center text-center py-10">
         <div className="flex justify-center">
-          <AvatarOnline image={userProfile.profilePictureUrl} />
+          <AvatarOnline image="https://images.pexels.com/photos/1043471/pexels-photo-1043471.jpeg?auto=compress&cs=tinysrgb&w=600" />
         </div>
         <div className="p-1">
           <h1 className="font-medium text-[16px] text-white">
-            {userProfile.name}
+            Tharindu Kasthuri
           </h1>
           <h3 className="font-semibold text-gray-500 text-[12px]">
-            {userProfile.profileStatus}
+            tharindukasthurisinghe@gmail.com
           </h3>
         </div>
       </div>
