a636c5bbeb04ffecdeb520679635a33d8ea6f8bc
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-15 06:40:00 +0530

    WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import ProtectedRoute from "./components/ProtectedRoute";
+// import ProtectedRoute from "./components/ProtectedRoute";
 import {
   BaseLayout,
   Home,
