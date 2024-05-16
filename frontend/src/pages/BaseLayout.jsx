e62fabe02da0db2bba0d12dc1846d8b0f3231ce4
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-16 19:15:16 +0530

    FIX: fixed the location where the user data is fetched

--- frontend/src/pages/BaseLayout.jsx
+++ frontend/src/pages/BaseLayout.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../api";
 import { Outlet } from "react-router-dom";
 import LeftBar from "../components/LeftBar";
 import NavBar from "../components/NavBar";
 
 const BaseLayout = () => {
+  const [profileData, setProfileData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      if (!localStorage.getItem("ACCESS_TOKEN")) {
+        setIsLoading(false);
+        return;
+      }
+
+      try {
+        const response = await api.get("/user/profile/");
+        setProfileData(response.data.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="flex h-screen">
       <div className="hidden xs:block w-[290px] bg-gray-200">
-        <LeftBar />
+        {profileData && <LeftBar profileData={profileData} />}
       </div>
       <div className="flex-1 flex flex-col">
         <div className="w-full bg-gray-200">
