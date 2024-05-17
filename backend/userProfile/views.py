d029356097b2271836f7837e987121eca9f9d594
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-17 11:01:34 +0530

    Backend fetch user data (#7)
    * WIP
    
    * FIX: Fixed the Rendering probelm
    
    * FIX: fixed the location where the user data is fetched
    
    * WIP
    
    * FEAT: Add user profile view and user login view
    
    * FIX: Fixing the issue with the user profile view
    
    * FIX: Fixing the response json data structure
    
    * FEAT: Add user profile view and url

--- backend/userProfile/views.py
+++ backend/userProfile/views.py
@@ -1,12 +1,151 @@
-from rest_framework import generics
-from .serializers import UserProfileSerializer
+from django.db import transaction
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer, UserProfileSerializer
 from .models import UserProfile
-from rest_framework.permissions import AllowAny
-
-# Create your views here.
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.exceptions import NotFound
 
 
 class UserCreateView(generics.CreateAPIView):
-    queryset = UserProfile.objects.all()
-    serializer_class = UserProfileSerializer
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+
+        profile = UserProfile.objects.get(user=serializer.instance)
+        profile_serializer = UserProfileSerializer(profile)
+
+        response_data = {
+            "status": "success",
+            "data": {
+                **serializer.data,
+                "profile": profile_serializer.data,
+            },
+        }
+        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        user_id = self.kwargs["pk"]
+        try:
+            user = User.objects.get(pk=user_id)
+            return user
+        except User.DoesNotExist:
+            return None
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance is None:
+            return Response(
+                {
+                    "status": "error",
+                    "message": "User not found",
+                },
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = self.get_serializer(instance)
+        profile = UserProfile.objects.get(user=instance)
+        profile_serializer = UserProfileSerializer(profile)
+
+        response_data = {
+            "status": "success",
+            "data": {
+                "id": serializer.data["id"],
+                "first_name": serializer.data["first_name"],
+                "last_name": serializer.data["last_name"],
+                "username": serializer.data["username"],
+                "email": serializer.data["email"],
+                "profile": profile_serializer.data,
+            },
+        }
+
+        return Response(response_data)
+
+
+class UserProfileDetailView(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        user = self.request.user
+        try:
+            profile = UserProfile.objects.get(user=user)
+            return profile
+        except UserProfile.DoesNotExist:
+            raise NotFound("User Profile not found")
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        user_data = {
+            "user": {
+                "id": instance.user.id,
+                "first_name": instance.user.first_name,
+                "last_name": instance.user.last_name,
+                "username": instance.user.username,
+                "email": instance.user.email,
+            }
+        }
+
+        # Combine profile and user data
+        response_data = {
+            "status": "success",
+            "data": {**serializer.data, **user_data},
+        }
+
+        return Response(response_data)
+
+
+class UserLoginView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            profile = UserProfile.objects.get(user=user)
+            serializer = UserProfileSerializer(profile)
+            response_data = {
+                "id": user.id,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "username": user.username,
+                "email": user.email,
+                "profile": serializer.data,
+            }
+
+            return Response(
+                {
+                    "status": "success",
+                    "message": response_data,
+                    "auth_tokens": {
+                        "refresh": str(refresh),
+                        "access": str(refresh.access_token),
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"status": "error", "message": "Invalid Credentials"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
--- frontend/src/pages/BaseLayout.jsx
+++ frontend/src/pages/BaseLayout.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "../api";
 import { Outlet } from "react-router-dom";
 import LeftBar from "../components/LeftBar";
 import NavBar from "../components/NavBar";
 
 const BaseLayout = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [profileData, setProfileData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      if (!localStorage.getItem("ACCESS_TOKEN")) {
+        setIsLoading(false);
+        return;
+      }
+
+      try {
+        const response = await api.get("/user/profile/");
+        setProfileData(response.data.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="flex h-screen">
       <div className="hidden xs:block w-[290px] bg-gray-200">
-        <LeftBar />
+        {/* {console.log(profileData)} */}
+        {profileData && <LeftBar profileData={profileData} />}
       </div>
       <div className="flex-1 flex flex-col">
         <div className="w-full bg-gray-200">
