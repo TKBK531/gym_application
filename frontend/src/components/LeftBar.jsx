673e60253df57a37376a82016edf115f343134cc
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-05-27 07:42:56 +0530

    FIX: Fixed the return json response structure (#13)

--- frontend/src/components/LeftBar.jsx
+++ frontend/src/components/LeftBar.jsx
@@ -8,9 +8,7 @@ const LeftBar = ({ profileData }) => {
     <div className=" w-[290px] bg-info-dark-blue h-screen">
       <div className="items-center text-center py-10">
         <div className="flex justify-center">
-          {profileData && (
-            <AvatarOnline image={profileData.profile.profile_picture} />
-          )}
+          {profileData && <AvatarOnline image={profileData.profile_picture} />}
         </div>
         <div className="p-1">
           <h1 className="font-medium text-[16px] text-white">
