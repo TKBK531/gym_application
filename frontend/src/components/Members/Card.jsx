2272652675447a3c16ded6a4fb924f2dee69e8e3
Sathija2000 <sathijabhanukadeshapriya@gmail.com>
2024-05-29 15:09:35 +0530

    FIX : add photos to the cards

--- frontend/src/components/Members/Card.jsx
+++ frontend/src/components/Members/Card.jsx
@@ -1,7 +1,9 @@
-const Card = ({content}) => {
+import PropTypes from 'prop-types';
+
+const Card = ({content, backgroundImage}) => {
   return (
     <div className="relative grid h-[20rem] w-full max-w-[28rem] flex-col items-end justify-center overflow-hidden rounded-xl bg-white bg-clip-border text-center text-gray-700">
-      <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
+      <div className="absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-cover bg-clip-border bg-center text-gray-700 shadow-none" style={{backgroundImage: `url(${backgroundImage})`,}}>
         <div className="to-bg-black-10 absolute inset-0 h-full w-full bg-gradient-to-t from-black/80 via-black/50"></div>
       </div>
       <div className="relative p-6 py-14 px-6 md:px-12">
@@ -14,6 +16,10 @@ const Card = ({content}) => {
       </div>
     </div>
   );
+};
+
+Card.propTypes = {
+  backgroundImage: PropTypes.string.isRequired,
 };
 
 export default Card;
--- frontend/src/pages/Members.jsx
+++ frontend/src/pages/Members.jsx
@@ -3,6 +3,11 @@ import StatWidge from '../components/Members/StatWidge';
 import { faSwimmer, faWalking, faUsers, faRunning  } from '@fortawesome/free-solid-svg-icons';
 
 const Members = () => {
+
+  const imageUrlPool = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-l9mhhOz1sK8P9IdEkk7seHj4APH_dRKQlg&s"
+  const imageUrlGround = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKTlfKV-33el_swtTqaO63TRfmeDEUb55zlA&s"
+  const imageUrlGym = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDWg1zq64xccwiMYVtHxmAty40QKKBxIahw&s"
+
   return (
     <div>
       <div>
@@ -20,9 +25,9 @@ const Members = () => {
           See who's joining the community, read about how our community are increasing their membership income and lot's more.​</p>
       </div>
       <div className="display: flex flex-row gap-4">
-        <Card content="High-tech gym for customized workouts"/>
-        <Card content="serene grounds for outdoor workouts"/>
-        <Card content="refreshing pool for aquatic exercise or relaxation"/>
+        <Card content="High-tech gym for customized workouts" backgroundImage={imageUrlGym}/>
+        <Card content="serene grounds for outdoor workouts" backgroundImage={imageUrlGround}/>
+        <Card content="refreshing pool for aquatic exercise or relaxation" backgroundImage={imageUrlPool}/>
       </div>
       <div className="container mx-auto px-4 py-8">
         <p className="text-gray-700 text-lg text-center leading-relaxed font-semibold">Apply today and embark on your personalized wellness journey.</p>
