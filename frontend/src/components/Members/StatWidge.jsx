258ec99b8177381e9b7d48376db27234e20ce1ab
Sathija2000 <88979641+Sathija2000@users.noreply.github.com>
2024-05-31 10:37:45 +0530

    Frontend membership (#16)
    * WIP
    
    * WIP
    
    * WIP : Creating members page card section
    
    * WIP : Working on statistic widge in members page
    
    * FIX : finished statistic widge frontend part
    
    * FIX : add photos to the cards

---
+++ frontend/src/components/Members/StatWidge.jsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const StatWidge = ({name, count, iconName}) => {
+  return (
+    <div className="w-full lg:w-1/4">
+      <div className="widget w-full p-4 rounded-lg bg-white border-l-4 border-yellow-400">
+        <div className="flex items-center">
+          <div className="icon w-14 p-3.5 bg-yellow-200 text-white rounded-full mr-3">
+            <div className="flex items-center justify-center">
+          <FontAwesomeIcon icon={iconName} className="text-blue-500" size="2x" />
+        </div>
+          </div>
+          <div className="flex flex-col justify-center">
+            <div className="text-lg">{ count }</div>
+            <div className="text-sm text-gray-400">{ name }</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StatWidge;
