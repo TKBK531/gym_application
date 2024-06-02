e65ad5da983513c7add3fed200ce206528b04eac
Sathija2000 <sathijabhanukadeshapriya@gmail.com>
2024-06-02 21:45:08 +0530

    FIX: members Page

--- frontend/src/pages/Members.jsx
+++ frontend/src/pages/Members.jsx
@@ -9,7 +9,6 @@ import {
 import { Link } from "react-router-dom";
 
 const Members = () => {
-<<<<<<< HEAD
   const imageUrlPool =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-l9mhhOz1sK8P9IdEkk7seHj4APH_dRKQlg&s";
   const imageUrlGround =
@@ -17,13 +16,6 @@ const Members = () => {
   const imageUrlGym =
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDWg1zq64xccwiMYVtHxmAty40QKKBxIahw&s";
 
-=======
-
-  const imageUrlPool = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-l9mhhOz1sK8P9IdEkk7seHj4APH_dRKQlg&s"
-  const imageUrlGround = "https://site.pdn.ac.lk/student/sprtpdn/data1/images/cricketground.jpg"
-  const imageUrlGym = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKDWg1zq64xccwiMYVtHxmAty40QKKBxIahw&s"
-  
->>>>>>> 75cbaf5 (WIP : designing the membership type pages)
   return (
     <div className="container mx-auto px-4 py-8">
       <div>
