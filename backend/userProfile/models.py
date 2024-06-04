df4ab729d8af9458f477950d5d4ec6878474cd83
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-06-04 10:07:20 +0530

    FEAT:Added Province and City Models and fixtures

--- backend/userProfile/models.py
+++ backend/userProfile/models.py
@@ -3,6 +3,21 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class Province(models.Model):
+    lable = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.lable
+
+
+class City(models.Model):
+    lable = models.CharField(max_length=50)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.lable
+
+
 class UserType(models.Model):
     name = models.CharField(max_length=50)
 
@@ -15,6 +30,7 @@ class UserProfile(models.Model):
     contact = models.CharField(max_length=10)
     profile_picture = models.TextField(null=True, blank=True)
     user_type = models.ForeignKey(UserType, on_delete=models.CASCADE, default=1)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}'s Profile"
