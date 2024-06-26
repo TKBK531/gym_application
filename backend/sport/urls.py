15aa906da8fce79fe2970e3232199572c9e668c2
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-06-26 21:35:51 +0530

    WIP

--- backend/sport/urls.py
+++ backend/sport/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SportListView
+from .views import SportListView, UpdateInChargeView
 
 urlpatterns = [
     path("all-sports/", SportListView.as_view(), name="sport-list"),
+    path("assign-in-charge/", UpdateInChargeView.as_view(), name="assign-in-charge"),
 ]
--- backend/sport/views.py
+++ backend/sport/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
-from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticated
 from rest_framework import generics
 from .models import Sport
 from .serializers import SportSerializer
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -13,17 +15,34 @@ class SportListView(generics.ListAPIView):
     permission_classes = [AllowAny]
 
 
-class AssignInCharge(generics.UpdateAPIView):
+class UpdateInChargeView(generics.UpdateAPIView):
     queryset = Sport.objects.all()
     serializer_class = SportSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
 
-    def get_user(pk):
+    def get_user(user_id):
         try:
-            in_charge = User.objects.get(pk=pk)
-
+            in_charge = User.objects.get(id=user_id)
+            if in_charge.groups.filter(name="staff").exists():
+                return in_charge
+            else:
+                raise ValidationError("User is not a staff member")
         except User.DoesNotExist:
             return None
 
     def perform_update(self, serializer):
-        serializer.save(in_charge=self.request.user)
+        if self.request.user.groups.filter(name="admin").exists():
+            user = self.get_user(self.request.data.get("in_charge"))
+            in_charge = self.get_user(user)
+            serializer.save(in_charge=in_charge)
+            resp_data = {
+                "status": "success",
+                "message": "In charge updated successfully",
+            }
+        else:
+            resp_data = {
+                "status": "error",
+                "message": "You are not authorized to perform this action",
+            }
+
+        return JsonResponse(resp_data)
