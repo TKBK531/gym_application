06edced05486746cae276ad774d6ecfd88bd058a
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-07-18 15:30:18 +0530

    FIX: Tried to fix the user profile picture not showing probelm if logged in using google account

--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -17,13 +17,27 @@ def generate_unique_identifier():
 
 
 def get_profile_picture_path(user_profile):
+    # host_url = settings.BASE_API_URL
+    # profile_picture = (
+    #     host_url + user_profile.profile_picture.url
+    #     if user_profile.profile_picture
+    #     and not user_profile.profile_picture.url.startswith("/media/https")
+    #     else (
+    #         user_profile.profile_picture.url if user_profile.profile_picture else None
+    #     )
+    # )
+    # print(f"----------{user_profile.profile_picture.url}")
+    # return profile_picture
     host_url = settings.BASE_API_URL
-    profile_picture = (
-        host_url + user_profile.profile_picture.url
-        if user_profile.profile_picture
-        and not user_profile.profile_picture.url.startswith("https")
-        else (
-            user_profile.profile_picture.url if user_profile.profile_picture else None
-        )
-    )
-    return profile_picture
+
+    if user_profile.profile_picture:
+        profile_picture_url = user_profile.profile_picture.url
+
+        if profile_picture_url.startswith("/media/https"):
+            profile_picture_url = profile_picture_url.replace("/media/", "", 1)
+        elif not profile_picture_url.startswith("http"):
+            profile_picture_url = host_url + profile_picture_url
+    else:
+        profile_picture_url = None
+    print(profile_picture_url)
+    return profile_picture_url
