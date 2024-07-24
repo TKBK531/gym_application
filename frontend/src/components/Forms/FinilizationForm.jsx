086b67a72415272088c778d8fa7cbbb9307a101f
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-07-24 18:39:49 +0530

    FIX: Final adjustments on finilization forms

--- frontend/src/components/Forms/FinilizationForm.jsx
+++ frontend/src/components/Forms/FinilizationForm.jsx
@@ -1,211 +1,3 @@
-// import PropTypes from "prop-types";
-// import { useState, useEffect } from "react";
-// import { faculties } from "../../constants/index";
-// import { Formik, Form, Field, ErrorMessage } from "formik";
-// import { formStyles } from "../../styles";
-
-// const FinilizationForm = ({ userData, selectedOption }) => {
-//   const [faculty, setFaculty] = useState("");
-//   const [userName, setUserName] = useState("");
-
-//   useEffect(() => {
-//     if (userData.email) {
-//       const faculty = getFacultyFromEmail(userData.email);
-//       const userName = getUsernameFromEmail(userData.email);
-//       if (faculty) {
-//         setFaculty(faculty.label);
-//       }
-//       if (userName) {
-//         setUserName(userName.toUpperCase());
-//       }
-//     }
-//   }, [userData.email]);
-
-//   const getUsernameFromEmail = (email) => {
-//     const username = email.split("@")[0];
-//     return username;
-//   };
-
-//   const getFacultyFromEmail = (email) => {
-//     const facultyCode = email.split("@")[1].split(".")[0];
-//     const faculty = faculties.find((faculty) => faculty.code === facultyCode);
-//     setFaculty(faculty.label);
-//     return faculty;
-//   };
-
-//   const finishButtonClicked = async () => {
-//     console.log("Finish Button Clicked");
-//   };
-
-//   return (
-//     <Formik
-//       // initialValues={initialValues}
-//       //   validationSchema={validationSchema}
-//       onSubmit={finishButtonClicked}
-//     >
-//       <Form action="" className="w-full">
-//         <div className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-md">
-//           {selectedOption === "student" && (
-//             <div className="flex flex-col gap-5">
-//               <div>
-//                 <label htmlFor="email" className={`${formStyles.formLabel}`}>
-//                   Registration Number
-//                 </label>
-//                 <Field
-//                   name="reg_number"
-//                   type="text"
-//                   disabled
-//                   value={userName}
-//                   className={`mt-1 block w-full px-3 py-2 border ${formStyles.formTextInput}`}
-//                 />
-//                 <ErrorMessage
-//                   name="reg_number"
-//                   component="div"
-//                   className={`${formStyles.formError}`}
-//                 />
-//               </div>
-//               <div>
-//                 <label htmlFor="email" className={`${formStyles.formLabel}`}>
-//                   Faculty
-//                 </label>
-//                 <Field
-//                   name="faculty"
-//                   type="text"
-//                   disabled
-//                   value={faculty}
-//                   className={`mt-1 block w-full px-3 py-2 border ${formStyles.formTextInput}`}
-//                 />
-//                 <ErrorMessage
-//                   name="faculty"
-//                   component="div"
-//                   className={`${formStyles.formError}`}
-//                 />
-//               </div>
-//             </div>
-//           )}
-//           {selectedOption === "academic" && (
-//             <div className="flex flex-col gap-5">
-//               <div>
-//                 <label htmlFor="email" className={`${formStyles.formLabel}`}>
-//                   UPF Number
-//                 </label>
-//                 <Field
-//                   name="upf_number"
-//                   type="text"
-//                   className={`mt-1 block w-full px-3 py-2 border ${formStyles.formTextInput}`}
-//                 />
-//                 <ErrorMessage
-//                   name="upf_number"
-//                   component="div"
-//                   className={`${formStyles.formError}`}
-//                 />
-//               </div>
-//               <div>
-//                 <label htmlFor="faculty" className={`${formStyles.formLabel}`}>
-//                   Faculty
-//                 </label>
-//                 <Field
-//                   name="faculty"
-//                   as="select"
-//                   className={`mt-1 block w-full px-3 py-2 border ${formStyles.formTextInput}`}
-//                   onChange={(e) => {
-//                     setFaculty(e.target.label);
-//                   }}
-//                 >
-//                   {console.log(faculty)}
-//                   <option value="" label="Select Faculty" />
-//                   {faculties.map((faculty) => (
-//                     <option key={faculty.pk} value={faculty.value}>
-//                       {faculty.label}
-//                     </option>
-//                   ))}
-//                 </Field>
-//                 <ErrorMessage
-//                   name="email"
-//                   component="div"
-//                   className={`${formStyles.formError}`}
-//                 />
-//               </div>
-//               <div>
-//                 <label
-//                   htmlFor="date_of_appointment"
-//                   className={`${formStyles.formLabel}`}
-//                 >
-//                   Date of Appoinment
-//                 </label>
-//                 <Field
-//                   name="date_of_appointment"
-//                   type="date"
-//                   className={`${formStyles.formTextInput}`}
-//                 />
-//                 <ErrorMessage
-//                   name="date_of_appointment"
-//                   component="div"
-//                   className={`${formStyles.formError}`}
-//                 />
-//               </div>
-//             </div>
-//           )}
-//           {selectedOption === "postgraduate" && (
-//             <div className="flex flex-col gap-5">
-//               <div>
-//                 <label
-//                   htmlFor="pg_registration_number"
-//                   className={`${formStyles.formLabel}`}
-//                 >
-//                   Postgraduate Registration Number
-//                 </label>
-//                 <Field
-//                   name="pg_registration_number"
-//                   type="text"
-//                   className={`mt-1 block w-full px-3 py-2 border ${formStyles.formTextInput}`}
-//                 />
-//                 <ErrorMessage
-//                   name="pg_registration_number"
-//                   component="div"
-//                   className={`${formStyles.formError}`}
-//                 />
-//               </div>
-
-//               <div>
-//                 <label
-//                   htmlFor="pg_commencement_date"
-//                   className={`${formStyles.formLabel}`}
-//                 >
-//                   Postgraduate Commencement Date
-//                 </label>
-//                 <Field
-//                   name="pg_commencement_date"
-//                   type="date"
-//                   className={`${formStyles.formTextInput}`}
-//                 />
-//                 <ErrorMessage
-//                   name="pg_commencement_date"
-//                   component="div"
-//                   className={`${formStyles.formError}`}
-//                 />
-//               </div>
-//             </div>
-//           )}
-//           <button
-//             type="submit"
-//             className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition duration-300 ease-in-out mt-5 w-full"
-//           >
-//             Finish
-//           </button>
-//         </div>
-//       </Form>
-//     </Formik>
-//   );
-// };
-
-// FinilizationForm.propTypes = {
-//   userData: PropTypes.object.isRequired,
-//   selectedOption: PropTypes.string.isRequired,
-// };
-
-// export default FinilizationForm;
-
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import { faculties } from "../../constants/index";
@@ -214,7 +6,7 @@ import * as Yup from "yup";
 import { formStyles } from "../../styles";
 
 const FinilizationForm = ({ userData, selectedOption }) => {
-  const [faculty, setFaculty] = useState("");
+  const [faculty, setFaculty] = useState(null);
   const [userName, setUserName] = useState("");
 
   useEffect(() => {
@@ -222,7 +14,7 @@ const FinilizationForm = ({ userData, selectedOption }) => {
       const faculty = getFacultyFromEmail(userData.email);
       const userName = getUsernameFromEmail(userData.email);
       if (faculty) {
-        setFaculty(faculty.label);
+        setFaculty(faculty);
       }
       if (userName) {
         setUserName(userName.toUpperCase());
@@ -243,7 +35,7 @@ const FinilizationForm = ({ userData, selectedOption }) => {
 
   const initialValues = {
     reg_number: userName,
-    faculty: faculty,
+    faculty: faculty ? faculty.label : "",
     upf_number: "",
     date_of_appointment: "",
     pg_registration_number: "",
@@ -273,7 +65,7 @@ const FinilizationForm = ({ userData, selectedOption }) => {
     if (selectedOption === "student") {
       const dataForBackend = {
         reg_number: userName,
-        faculty: faculty,
+        faculty: faculty.pk,
       };
       console.log(dataForBackend);
     }
@@ -281,7 +73,7 @@ const FinilizationForm = ({ userData, selectedOption }) => {
       const dataForBackend = {
         upf_number: values.upf_number,
         date_of_appointment: values.date_of_appointment,
-        faculty: values.faculty,
+        faculty: faculty.pk,
       };
       console.log(dataForBackend);
     }
@@ -336,7 +128,7 @@ const FinilizationForm = ({ userData, selectedOption }) => {
                     name="faculty"
                     type="text"
                     disabled
-                    value={faculty}
+                    value={faculty ? faculty.label : ""}
                     className={`mt-1 block w-full px-3 py-2 border ${formStyles.formTextInput}`}
                   />
                   <ErrorMessage
@@ -379,8 +171,11 @@ const FinilizationForm = ({ userData, selectedOption }) => {
                     as="select"
                     className={`mt-1 block w-full px-3 py-2 border ${formStyles.formTextInput}`}
                     onChange={(e) => {
+                      const selectedFaculty = faculties.find(
+                        (faculty) => faculty.label === e.target.value
+                      );
+                      setFaculty(selectedFaculty);
                       setFieldValue("faculty", e.target.value);
-                      setFaculty(e.target.value);
                     }}
                   >
                     <option value="" label="Select Faculty" />
