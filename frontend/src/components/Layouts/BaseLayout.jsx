b8a5b04527e6fe8cf94f3cc7e5e2a2c55c7e2706
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-07-25 22:52:25 +0530

    FIX: Finished the mobile responsive leftbar design

--- frontend/src/components/Layouts/BaseLayout.jsx
+++ frontend/src/components/Layouts/BaseLayout.jsx
@@ -1,31 +1,4 @@
-// import { Outlet } from "react-router-dom";
-// import NavBar from "../NavBar";
-// import Leftbar from "../NavComponents/Leftbar";
-// import ScrollToTopButton from "../Buttons/ScrollToTopButton";
-
-// const BaseLayout = () => {
-//   const userData = JSON.parse(localStorage.getItem("userData"));
-
-//   return (
-//     <div className="grid h-screen grid-rows-[auto,1fr]">
-//       <div className="w-full bg-gray-200">
-//         <NavBar />
-//       </div>
-//       <div className="grid grid-cols-[auto,1fr]">
-//         <div className="bg-gray-200 h-screen overflow-y-auto">
-//           <Leftbar userData={userData} />
-//         </div>
-//         <div className="bg-slate-100 flex flex-col py-14 px-7">
-//           <Outlet />
-//         </div>
-//       </div>
-//       <ScrollToTopButton />
-//     </div>
-//   );
-// };
-
-// export default BaseLayout;
-import React, { useState } from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "../NavBar";
 import Leftbar from "../NavComponents/Leftbar";
@@ -44,16 +17,8 @@ const BaseLayout = () => {
       <div className="w-full bg-gray-200">
         <NavBar onHamburgerClick={handleHamburgerClick} />
       </div>
-      <div className="flex flex-1 overflow-hidden w-fit">
-        <div
-          className={`bg-gray-200 md:block ${
-            isLeftbarVisible ? "block" : "hidden"
-          } absolute z-10 md:relative w-64 md:w-auto transition-transform transform md:translate-x-0 ${
-            isLeftbarVisible ? "translate-x-0" : "-translate-x-full"
-          }`}
-        >
-          <Leftbar userData={userData} isLeftbarVisible={isLeftbarVisible} />
-        </div>
+      <div className="flex flex-1 overflow-hidden w-full">
+        <Leftbar userData={userData} isLeftbarVisible={isLeftbarVisible} />
         <div className="flex-1 bg-slate-100 flex flex-col py-14 px-7 overflow-auto">
           <Outlet />
         </div>
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,67 +1,6 @@
-// import React from "react";
-// import { useLocation, NavLink } from "react-router-dom";
-
-// const NavBar = () => {
-//   const location = useLocation();
-
-//   const getNavPath = (pathname) => {
-//     const parts = pathname.split("/").filter(Boolean);
-
-//     if (parts.length === 0) {
-//       return (
-//         <NavLink to="/" className="text-white hover:underline">
-//           Home
-//         </NavLink>
-//       );
-//     }
-
-//     return parts.map((part, index) => (
-//       <React.Fragment key={part}>
-//         {index > 0 && (
-//           <span className="text-black mx-1">&gt;</span> // Arrow separator
-//         )}
-//         <NavLink
-//           to={`/${parts.slice(0, index + 1).join("/")}`}
-//           className={({ isActive }) =>
-//             `text-inf hover:underline ${isActive ? "font-bold" : ""}`
-//           } // Highlight the active link
-//         >
-//           {part.charAt(0).toUpperCase() + part.slice(1)}
-//         </NavLink>
-//       </React.Fragment>
-//     ));
-//   };
-
-//   return (
-//     <nav className="bg-white p-4 sticky top-0">
-//       <div className="container mx-auto flex items-center">
-//         {/* Hamburger Menu Icon */}
-//         <button className="block md:hidden mr-4">
-//           <svg
-//             className="w-6 h-6"
-//             fill="none"
-//             stroke="currentColor"
-//             viewBox="0 0 24 24"
-//             xmlns="http://www.w3.org/2000/svg"
-//           >
-//             <path
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//               strokeWidth="2"
-//               d="M4 6h16M4 12h16m-7 6h7"
-//             ></path>
-//           </svg>
-//         </button>
-//         {/* Optional logo or brand name here */}
-//         <div className="ml-auto">{getNavPath(location.pathname)}</div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default NavBar;
 import React from "react";
 import { useLocation, NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
 
 const NavBar = ({ onHamburgerClick }) => {
   const location = useLocation();
@@ -119,6 +58,10 @@ const NavBar = ({ onHamburgerClick }) => {
       </div>
     </nav>
   );
+};
+
+NavBar.propTypes = {
+  onHamburgerClick: PropTypes.func.isRequired,
 };
 
 export default NavBar;
