3235dd3fd17ff4016e2164e4f7cb47fdf53104b2
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-07-25 10:06:26 +0530

    WIP

--- frontend/src/components/Layouts/BaseLayout.jsx
+++ frontend/src/components/Layouts/BaseLayout.jsx
@@ -2,16 +2,16 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../NavBar";
 import Leftbar from "../NavComponents/Leftbar";
 import ScrollToTopButton from "../Buttons/ScrollToTopButton";
-import Drawer from "../NavComponents/Drawer";
+// import Drawer from "../NavComponents/Drawer";
 
 const BaseLayout = () => {
   const userData = JSON.parse(localStorage.getItem("userData"));
 
   return (
     <div className="grid h-screen grid-cols-[auto,1fr]">
-      <div className="bg-gray-200 h-screen overflow-y-auto">
-        {/* <Leftbar userData={userData} /> */}
-        <Drawer />
+      <div className="bg-gray-200 h-screen overflow-y-auto hidden sm:visible">
+        <Leftbar userData={userData} />
+        {/* <Drawer /> */}
       </div>
       <div className="flex flex-col">
         <div className="w-full bg-gray-200">
