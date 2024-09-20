064f8199c84a93fcda37e6069cfc7175bfb7fae4
Sathija2000 <sathijabhanukadeshapriya@gmail.com>
2024-09-20 22:06:45 +0530

    FIX: components change into responsive

--- frontend/src/components/Table/FamilyDetails.jsx
+++ frontend/src/components/Table/FamilyDetails.jsx
@@ -115,7 +115,7 @@ const FamilyDetails = () => {
       <button
         type="button"
         onClick={handleAddMember}
-        className="mt-4 px-2 py-1 bg-blue-300 text-white rounded hover:bg-blue-400"
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
       >
         Add Member
       </button>
