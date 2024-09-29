44ccd8114188a37356258473ecd9549e76cab085
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2024-09-29 19:14:16 +0530

    FIX: Fixed the routes

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,9 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import GoogleLoginLoading from "./components/Loading/GoogleLoginLoading";
-import SportCardPage from "./components/Sport/SportCardPage";
 import {
-  BaseLayout,
+  // BaseLayout,
   Home,
   Events,
   Items,
@@ -16,6 +15,12 @@ import {
   PageNotFound,
 } from "./pages";
 import Register from "./pages/Register";
+import BaseLayout from "./components/Layouts/BaseLayout";
+import MembersGym from "./pages/MembersGym";
+import MembersGround from "./pages/MembersGround";
+import MembersPool from "./pages/MembersPool";
+import Table from "./components/Table";
+import SportCardPage from "./components/Sport/SportCardPage";
 
 export const router = createBrowserRouter([
   {
@@ -32,7 +37,6 @@ export const router = createBrowserRouter([
       },
     ],
   },
-
   {
     path: "/events",
     element: <BaseLayout />,
@@ -87,14 +91,38 @@ export const router = createBrowserRouter([
           </ProtectedRoute>
         ),
       },
+      {
+        path: "membersGym",
+        element: (
+          <ProtectedRoute>
+            <MembersGym />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "membersGround",
+        element: (
+          <ProtectedRoute>
+            <MembersGround />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "membersPool",
+        element: (
+          <ProtectedRoute>
+            <MembersPool />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
   {
     path: "/sports",
     element: <BaseLayout />,
     children: [
-      { index: true, element: <Sports /> }, // Route for the main Sports list
-      { path: ":sportName", element: <SportCardPage /> }, // Dynamic route for sport pages
+      { index: true, element: <Sports /> },
+      { path: ":sportName", element: <SportCardPage /> },
     ],
   },
   {
@@ -111,7 +139,20 @@ export const router = createBrowserRouter([
       },
     ],
   },
-  // Single routes not requiring nested structure
+  {
+    path: "/table",
+    element: <BaseLayout />,
+    children: [
+      {
+        index: true,
+        element: (
+          <ProtectedRoute>
+            <Table />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
   { path: "/loading", element: <GoogleLoginLoading /> },
   { path: "/login", element: <Login /> },
   { path: "/logout", element: <Logout /> },
