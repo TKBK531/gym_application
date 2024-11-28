9cafdaa50a0b76c1f2da20378498d0a34efc10b0
Sathija <sathijabhanukadeshapriya@gmail.com>
2024-11-28 23:06:13 +0530

    FIX : create urls for member app

--- backend/member/urls.py
+++ backend/member/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import *
+from .views import (
+    CreateMemberView,
+    CreatePostgraduateMemberView,
+    CreateAcademicStaffMemberView,
+    CreateOutsidersMemberView,
+    CreateFamilyView,
+    CreateFamilyMemberView,
+)
 
 urlpatterns = [
-    path('', home),
+    path("members/create/", CreateMemberView.as_view(), name="create-member"),
+    path("postgraduate-members/create/", CreatePostgraduateMemberView.as_view(), name="create-postgraduate-member"),
+    path("academic-staff-members/create/", CreateAcademicStaffMemberView.as_view(), name="create-academic-staff-member"),
+    path("families/create/", CreateFamilyView.as_view(), name="create-family"),
+    path("family-members/create/", CreateFamilyMemberView.as_view(), name="create-family-member"),
+    path("outsiders-members/create/", CreateOutsidersMemberView.as_view(), name="create-outsiders-member"), 
 ]
