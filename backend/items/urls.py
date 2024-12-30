ae36859143ebb363b19b3a73b9121e03aa0f4d5f
HasarangaDinujaya <hasarangadinujaya1234@gmail.com>
2024-12-30 23:59:27 +0530

    Add Endpoints

--- backend/items/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ItemCreateView, ItemListView
-
-urlpatterns = [
-    path('add/', ItemCreateView.as_view(), name='item-add'),
-    path('all-items/', ItemListView.as_view(), name='item-list'),  # Add the URL to access the item list
-]
-
-
