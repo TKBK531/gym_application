47b70f6c061642cf3a5a3fea7e7623791f8985ff
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2025-01-01 09:28:36 +0530

    FIX: Created the integration for the events table

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import api from '../api';
 
 const Table = ({ userRole, selectedCategory }) => {
   const [searchTerm, setSearchTerm] = useState('');
