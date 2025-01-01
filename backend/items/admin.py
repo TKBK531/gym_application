3b02a2d1926d1d0d6d699286e6b5603f6d25e2ca
HasarangaDinujaya <hasarangadinujaya1234@gmail.com>
2025-01-01 09:59:34 +0530

    WIP

--- backend/items/admin.py
+++ backend/items/admin.py
@@ -1,36 +1,27 @@
-# from django.contrib import admin
-# from .models import ItemType, Item, inUse
-
-
-# # Register your models here.
-# @admin.register(ItemType)
-# class ItemTypeAdmin(admin.ModelAdmin):
-#     list_display = ("id", "label", "sport")
-#     list_filter = ("sport",)
-
-
-# @admin.register(Item)
-# class ItemAdmin(admin.ModelAdmin):
-#     list_display = ("id", "item_id", "item_type", "sport")
-#     list_filter = ("sport",)
-
-
-# @admin.register(inUse)
-# class inUseAdmin(admin.ModelAdmin):
-#     list_display = ("id", "item", "user")
-#     list_filter = ("item",)
-
 from django.contrib import admin
-from .models import ItemType, Item, InUse
+from .models import Equipment, ItemType, Item, InUse
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ('item', 'sport', 'count')
+    search_fields = ('item',)
+    list_filter = ('sport',)
+    ordering = ('item',)
 
 @admin.register(ItemType)
 class ItemTypeAdmin(admin.ModelAdmin):
     list_display = ('name',)
+    search_fields = ('name',)
 
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'item_type', 'count')
+    search_fields = ('name', 'item_type__name')
+    list_filter = ('item_type',)
+    ordering = ('name',)
 
 @admin.register(InUse)
 class InUseAdmin(admin.ModelAdmin):
     list_display = ('item', 'is_in_use')
+    search_fields = ('item__name',)
+    list_filter = ('is_in_use',)
