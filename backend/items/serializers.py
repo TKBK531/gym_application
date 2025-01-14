265712528ba279f868bb1599eb1c2e9d3e7beb7b
HasarangaDinujaya <hasarangadinujaya1234@gmail.com>
2025-01-14 23:51:58 +0530

    Update the endpoints

--- backend/items/serializers.py
+++ backend/items/serializers.py
@@ -1,18 +1,15 @@
-# from rest_framework import serializers
-# from .models import Item
-
-
-# class ItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Item
-#         fields = ['item_id', 'item_type', 'sport', 'count'] # Include all necessary fields
-
-#     def create(self, validated_data):
-#         return Item.objects.create(**validated_data)
-    
-# serializers.py
 from rest_framework import serializers
-from .models import Equipment
+from .models import Item , Equipment
+
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ['item_id', 'item_type', 'sport', 'count'] # Include all necessary fields
+
+    def create(self, validated_data):
+        return Item.objects.create(**validated_data)
+    
 
 class EquipmentSerializer(serializers.ModelSerializer):
     class Meta:
