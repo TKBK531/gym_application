2e788588527f475de14e575ace7728f9c3ff2ed2
HasarangaDinujaya <hasarangadinujaya1234@gmail.com>
2025-01-19 00:56:46 +0530

    FEAT : Update models

--- backend/items/urls.py
+++ backend/items/urls.py
@@ -1,11 +1,3 @@
-# from django.urls import path
-# from .views import ItemCreateView, ItemListView
-
-# urlpatterns = [
-#     path('add/', ItemCreateView.as_view(), name='item-add'),
-#     path('all-items/', ItemListView.as_view(), name='item-list'),  # Add the URL to access the item list
-# ]
-
 # urls.py
 from django.urls import path
 from .views import EquipmentListView, EquipmentCreateView, EquipmentUpdateView, EquipmentDeleteView
