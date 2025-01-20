ffc35f157070a00f7e54d9243ee5763d90dc584e
HasarangaDinujaya <hasarangadinujaya1234@gmail.com>
2025-01-20 23:05:30 +0530

    FEAT: Create endpoints

--- backend/studentData/models.py
+++ backend/studentData/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+
 class Student(models.Model):
     name = models.CharField(max_length=100)
     registration_number = models.CharField(max_length=50, unique=True)
     phone_number = models.CharField(max_length=10)
     faculty = models.CharField(max_length=100)
-    sports_experiences = models.TextField()
-    achievements = models.JSONField()  # Store achievements as an array of strings
+    sports_experiences = models.TextField(blank=True, null=True)
+    achievements = models.JSONField(
+        default=list
+    )  # Store achievements as an array of strings
 
     def __str__(self):
         return self.name
--- backend/studentData/urls.py
+++ backend/studentData/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import StudentCreateView , StudentListView
+from .views import StudentCreateView, StudentListView
 
 
 urlpatterns = [
-    path('studentsList/', StudentCreateView.as_view(), name='student-create'),
-    path('studentsView/', StudentListView.as_view(), name='student-list'),
+    path("studentsList/", StudentCreateView.as_view(), name="student-create"),
+    path("studentsView/", StudentListView.as_view(), name="student-list"),
 ]
