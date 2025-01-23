2ee44d4e0494103ef17e3edf004c0fe8c8cd72c2
Tharindu Kasthurisinghe <tharindukasthurisinghe@gmail.com>
2025-01-23 06:37:04 +0530

    FIX: Fix conflicts

--- backend/items/serializers.py
+++ backend/items/serializers.py
@@ -1,9 +1,5 @@
 from rest_framework import serializers
-<<<<<<< HEAD
 from .models import Item, Equipment
-=======
-from .models import Item , Equipment
->>>>>>> ab26698 (Update the endpoints)
 
 
 class ItemSerializer(serializers.ModelSerializer):
